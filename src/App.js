a2fd992717a7b27876b984650d3439a8fa4a541a
Baek-dev <travel_dob@naver.com>
2021-01-02 20:11:50 +0900

    props와 proptypes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // function Food(prop){
-function Food({favorite}){
+function Food({name, picture, rating}){
   // return <h1>I like {prop.favorite}</h1>;
-  return (
-    <div>
-      <h1> I love {favorite} </h1>
+  return <div>
+      <h1> I love {name} </h1>
+      <h4> rate {rating}</h4>
+      <img src={picture} alter={name} />
     </div>
-  );
+}
+
+//이곳에서 타입이 일치하지 않으면 콘솔에 warning이 표시됨\
+// propTypes라는 이름으로 지어야 함! 꼭! 안그러면 에러됨
+// 문서 https://reactjs-kr.firebaseapp.com/docs/typechecking-with-proptypes.html 참고
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "./logo192.png",
+    rating: 4
+  },
+  {
+    id: 2,
+    name: "bibimbap",
+    image: "./logo512.png",
+    rating: 3
+  },
+];
+
+// 이렇게 작성해서 써도 됨
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} rating={dish.rating} />
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
-      <Food favorite ="TTeok" />
-      <Food favorite ="kimchi" />
-      <Food favorite="Ramen"/>
-      <Food favorite="chukumi"/>
+      {/* dish는 object임 */}
+      {/* {foodILike.map(renderFood)} */}
+      {foodILike.map(dish =>(
+        // key는 리액트 내부에서 사용하기 위한 키로 이걸로 다른 컴포넌트라는것을 구별함 
+        // Food 함수에 전송하기 위함이 아님
+        <Food key={dish.id} name={dish.name} picture={dish.image}  rating={dish.rating} />
+      ))}
     </div>
   );
 }

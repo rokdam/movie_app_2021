1d1999547e84d21c4cb5185331d0119698454826
Baek-dev <travel_dob@naver.com>
2021-01-06 21:45:06 +0900

    component의 life cycle

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-// function Food(prop){
-function Food({name, picture, rating}){
-  // return <h1>I like {prop.favorite}</h1>;
-  return <div>
-      <h1> I love {name} </h1>
-      <h4> rate {rating}</h4>
-      <img src={picture} alter={name} />
-    </div>
-}
+//App은 react의 컴포넌트가 됨.
+//React.component에서 확장했기 때문에 react component로부터 모든 것을 가져오게 됨
+class App extends React.Component{
 
-//이곳에서 타입이 일치하지 않으면 콘솔에 warning이 표시됨\
-// propTypes라는 이름으로 지어야 함! 꼭! 안그러면 에러됨
-// 문서 https://reactjs-kr.firebaseapp.com/docs/typechecking-with-proptypes.html 참고
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  //js가 불러와질 때 가장 먼저 호출되는 함수(react X)
+  constructor(props){
+    super(props);
+    console.log("첫번째");
+  }
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image: "./logo192.png",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "bibimbap",
-    image: "./logo512.png",
-    rating: 3
-  },
-];
+  state = {
+    count : 0
+  };
+  
+  //js code function
+  add = () => {
+    //this.state.count는 좋지는 않음. state에 의존하고 싶지 않음. 성능 문제가 있음!!
+    // this.setState({count: this.state.count+1});
+    //state를 set 할 때, 외부의 state에 의존하지 않는 가장 좋은 방법임 current쓰기!
+    this.setState( current => ({count: current.count+1}));
+;  };
+  minus = () => {
+    this.setState({count: this.state.count-1});
+    console.log("minus");
+  };
 
-// 이렇게 작성해서 써도 됨
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image} rating={dish.rating} />
-}
+  //mount됐다고 알려주는 함수
+  componentDidMount(){
+    console.log("component rendered");
+  }
 
-function App() {
-  return (
-    <div>
-      {/* dish는 object임 */}
-      {/* {foodILike.map(renderFood)} */}
-      {foodILike.map(dish =>(
-        // key는 리액트 내부에서 사용하기 위한 키로 이걸로 다른 컴포넌트라는것을 구별함 
-        // Food 함수에 전송하기 위함이 아님
-        <Food key={dish.id} name={dish.name} picture={dish.image}  rating={dish.rating} />
-      ))}
-    </div>
-  );
+  //componentWillUnmount는 컴포넌트가 떠날 때 호출되는 함수임
+  //그렇기 때문에 안에 내용을 작성해도 콘솔에는 뜨지 않네ㅜ;
+  // componentWillUnmount{
+  // }
+
+  //update작업을 하고 나서 update됐다고 알려주는 함수
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+
+  render(){
+    console.log("I am rendering");
+    return <div> 
+        <h1>The number is : {this.state.count} </h1>
+        <button onClick={this.add}> Add</button>
+        <button onClick={this.minus}> Minus </button>
+      </div>
+  }
+
+  
 }
 
 export default App;
